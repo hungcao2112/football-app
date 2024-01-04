6d0c40a3e864ad9dbb02b9fea75279b781ad4732
Hung Cao <hungcao2112@gmail.com>
2024-01-05 05:07:24 +0700

    Initial Commit

---
+++ football-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  football-app
+//
+//  Created by Hung Cao on 05/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
